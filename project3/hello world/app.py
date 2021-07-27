d6d23c962abd2ff14e56d31a9f1fed930981cde1
Sufyan-Kamran <76629748+Sufyan-Kamran@users.noreply.github.com>
2021-07-27 12:44:26 -0700

    Add files via upload

--- project3/hello world/app.py
+++ project3/hello world/app.py
@@ -1,10 +1,10 @@
 #app.py
 from flask import Flask, render_template, request, redirect, url_for, session
-from flask_mysqldb import MySQL,MySQLdb #pip install flask-mysqldb https://github.com/alexferl/flask-mysqldb
-import bcrypt #pip install bcrypt https://pypi.org/project/bcrypt/
+from flask_mysqldb import MySQL,MySQLdb 
+import bcrypt 
 
 app = Flask(__name__)
-app.secret_key = "caircocoders-ednalan-2020"
+app.secret_key = "sufyan-kamran-3320"
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
